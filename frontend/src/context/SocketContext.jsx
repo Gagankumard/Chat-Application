e1a3f04c3c5a84bb8f2ff923c15b373c26ef0414
Gagankumard <gagank37851@gmail.com>
2024-07-08 14:34:31 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-5c54.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
